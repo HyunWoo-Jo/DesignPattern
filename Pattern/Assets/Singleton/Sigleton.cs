c1cb9f0674b126640c63cc16ea61dd76ecbc5cc8
HyunWooJo <CoreHyunWoo@gmail.com>
2020-11-12 05:45:41 +0900

    01.Sigleton
    01.Sigleton

---
+++ Pattern/Assets/Singleton/Sigleton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Sigleton<T> : MonoBehaviour where T : Sigleton<T>
+{
+    static T Instance;
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(this.gameObject);
+        } else {
+            Instance = this.GetComponent<T>();
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
