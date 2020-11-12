a41b36a6713e29782ab945c8bc06378ae83190b6
HyunWooJo <CoreHyunWoo@gmail.com>
2020-11-12 19:41:40 +0900

    State
    State Pattern

---
+++ Pattern/Assets/State/ConcreteStateB.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcreteStateB : State
+{
+    public override void Enter() {
+    }
+    public override void Exit() {
+    }
+
+}
