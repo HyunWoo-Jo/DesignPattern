89f25af69040d140909b47ca65d77572705e390f
HyunWooJo <CoreHyunWoo@gmail.com>
2020-11-13 16:52:19 +0900

    Strategy
    Strategy

---
+++ Pattern/Assets/Strategy/ConcreteStrategyB.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcreteStrategyB : MonoBehaviour, Strategy {
+    public void Execute() {
+   
+    }
+}
---
+++ Pattern/Assets/Strategy/Strategy.cs
@@ -0,0 +1,4 @@
+﻿public interface Strategy
+{
+    void Execute();
+}
