9a44c81efb5737f3f38c58bdfcffb6b4fcef9bd6
HyunWooJo <CoreHyunWoo@gmail.com>
2020-11-30 19:36:46 +0900

    Proxy
    주로 프록시 패턴은 RealSubject 가 원격 시스템에서 돌아가거나, 그 객체의 생성 비용이 많이 들어 실제 사용 시점에 객체를 생성하거나,
    
    실제 객체에 접근을 제한 및 제어를 해야 할 때 등 의 경우에 사용 됩니다.

---
+++ Pattern/Assets/Structural/Proxy/Proxy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proxy : MonoBehaviour, ProxySubject
+{
+    RealSubject real;
+    private void Awake() {
+        real = GetComponent<RealSubject>();
+    }
+    public void Action() {
+        real.Action();
+    }
+}
---
+++ Pattern/Assets/Structural/Proxy/RealSubject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealSubject : MonoBehaviour, ProxySubject
+{
+    public void Action() {
+
+    }
+
+}
