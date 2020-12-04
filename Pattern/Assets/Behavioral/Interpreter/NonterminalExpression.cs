1d6e9932a38abcbb6fcbee11a453fe04824f4799
HyunWooJo <CoreHyunWoo@gmail.com>
2020-12-04 23:20:44 +0900

    Interpreter
    문법 규칙을 클래스환 구조
    일련의 규칙으로 정의된 언어를 해석하는 패턴이다.
    규칙이 많아지면 복잡하고 무거워지기 때문에 파서 컴파일러 생성기를 쓰는게 좋다.
    예로 SQL 구문, 통신 프로토콜

---
+++ Pattern/Assets/Behavioral/Interpreter/NonterminalExpression.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonterminalExpression : MonoBehaviour, AbstractExpression
+{
+    public void Interpret(string con) {
+        Debug.Log("NontminalExpression:" + con);
+    }
+
+}
---
+++ Pattern/Assets/Behavioral/Interpreter/TerminalExpression.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalExpression : MonoBehaviour, AbstractExpression
+{
+    string data;
+    public TerminalExpression(string data) {
+        this.data = data;
+    }
+    public void Interpret(string con) {
+        Debug.Log("TerminalExpression:" + con);
+    }
+}
