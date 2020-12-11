97bed46bb1489921b86a539aa2725eb03141ce2b
HyunWooJo <CoreHyunWoo@gmail.com>
2020-12-11 22:17:35 +0900

    Iterator
    Iterator

---
+++ Pattern/Assets/Behavioral/Iterator/ConcreteAggregate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcreteAggregate : MonoBehaviour, Aggregate
+{
+    public void CreateIterator() {
+        Iterator iterator = GetComponent<Iterator>(); ;
+        iterator.Next();
+    }
+
+  
+}
---
+++ Pattern/Assets/Behavioral/Iterator/ConcreteIterator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcreteIterator : MonoBehaviour,Iterator
+{
+    public void HasNext() {
+
+    }
+
+    public void Next() {
+
+    }
+
+    public void Remove() {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
