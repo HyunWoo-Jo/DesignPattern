02729963ff6ddb14cf1dece6832c6ce37bd9403f
HyunWooJo <CoreHyunWoo@gmail.com>
2020-12-13 11:57:14 +0900

    Mediator
    Mediator

---
+++ Pattern/Assets/Behavioral/Mediator/ConcreteMediator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcreteMediator : Mediator
+{
+    PopColleagueB pop;
+    PushColleagueA push;
+
+    void Push() {
+        push.Execute();
+    }
+
+    void Pop() {
+        pop.Execute();
+    }
+}
---
+++ Pattern/Assets/Behavioral/Mediator/PushColleagueA.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushColleagueA : MonoBehaviour, Colleague {
+    public void Execute() {
+
+    }
+}
